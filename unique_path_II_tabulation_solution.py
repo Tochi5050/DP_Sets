64ca72a17b63b0dde9d0286c79d014047f38c1b3
Tochi5050 <ctochi46@gmail.com>
2025-02-09 23:22:25 +0100

    Working on the tabulation version of the unique paths II DP problem

---
+++ unique_path_II_tabulation_solution.py
@@ -0,0 +1,19 @@
+def uniquePathsII(grid):
+    m = len(grid)
+    n = len(grid[0])
+
+    if grid[0][0] == 1:
+        return 0
+    grid[0][0] = 1
+
+    for i in range(1, m):
+        # print(int(grid[i][0] == 0 and grid[i - 1][0] == 1))
+        grid[i][0] = int(grid[i][0] == 0 and grid[i - 1][0])
+
+    for j in range(1, n):
+        grid[0][j] = int(grid[0][j] == 0 and grid[0][j - 1] == 1)
+
+
+
+grid = [[0,0,0],[0,1,0],[0,0,0]]
+print(uniquePathsII(grid))
