4ceac35d4d6c890a13fa9382bb3fced8658e3fbb
Tochi5050 <ctochi46@gmail.com>
2025-03-16 17:31:28 +0100

    Adding climbing stairs bottom up solution

---
+++ climbing_stairs_bottom_up.py
@@ -0,0 +1,14 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        def climbStairsHelper(n):
+            stairs = [0] * (n + 1)
+
+            stairs[1] = 1
+            stairs[0] = 1
+
+            for i in range(2, n + 1):
+                stairs[i] = stairs[i - 1] + stairs[i - 2]
+
+            return stairs[n]
+
+        return climbStairsHelper(n)
